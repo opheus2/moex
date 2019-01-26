9a6377a98dda9a90721bc4f0fd3494af551dd00f
Shafi Abdulrahman <techissued@gmail.com>
2019-01-26 12:10:03 +0100

    update

---
+++ resources/views/layouts/landing.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="title" content="@yield('title')">
+    <meta name="description" content="@yield('meta-description')">
+    <link href='https://fonts.googleapis.com/css?family=Nunito:400,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="{{asset('css/landing.css')}}">
+    <title>@yield("title", config('app.name', 'Moex'))</title>
+    <!-- Fonts -->
+    <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css" integrity="sha384-PmY9l28YgO4JwMKbTvgaS7XNZJ30MK9FAZjjzXtlqyZCqBY6X6bXIkM++IkyinN+" crossorigin="anonymous">
+<!-- Latest compiled and minified CSS -->
+<!-- <link rel="stylesheet" href="https://max÷cdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"> -->
+
+
+@yield('custom_styles')
+</head>
+
+<body>
+
+    <div id="app">
+        @include('layouts.landing.header')
+            <main id="main" class="py-0" style="margin-top: -19px;">
+                @yield('content')
+            </main>
+        @include('layouts.landing.footer')
+
+
+
+    </body>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+@yield('custom_js')
+
+</html>
---
+++ resources/views/layouts/landing/header.blade.php
@@ -0,0 +1,43 @@
+<nav id="navbar" class="navbar header-bg">
+  <div class="container-fluid">
+    <div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" onclick="openNav()" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand header-brand"  href="#">Moex</a>
+    </div>
+    <div id="mySidenav" class="sidenav">
+      <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+      <a class="text-color" href="#">Buy Bitcoins</a>    
+      <a class="text-color" href="#">Sell Bitcoins</a>    
+      <a class="text-color" href="#">Forums</a>    
+      <a class="text-color" href="#">About</a>  
+      <a class="text-color btn btn-default btn-lg button-sign" href="#">Sign Up</a>  
+</div>
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right header-links">
+        <li><a class="text-color" href="#">Buy Bitcoins</a></li>
+        <li><a class="text-color" href="#">Sell Bitcoins</a></li>
+        <li><a class="text-color" href="#">Forums</a></li>
+        <li><a class="text-color" href="#">About</a></li>
+        <li><a class="text-color btn btn-default btn-lg button-sign" href="#">Sign Up</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<script>
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  // document.getElementById("main").style.marginLeft = "250px";
+  // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  // document.getElementById("main").style.marginLeft= "0";
+  // document.body.style.backgroundColor = "white";
+}
+</script>
