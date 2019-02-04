c95f67cb42e31cb23472991ba2edaf5e9286696f
James John James <jamesjay4199@gmail.com>
2019-02-04 23:54:22 +0100

    tables phase 2

--- resources/assets/js/components/TableView.vue
+++ resources/assets/js/components/TableView.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="">
+    <v-client-table :data="tableData" :columns="columns" :options="options"></v-client-table>
+    <!--<div class="">
       <div style="overflow-x:auto;">
           <table class="table table-hover table-responsive responsive" id="tableView" style="">
               <thead>
@@ -83,19 +84,66 @@
               </tbody>
           </table>
       </div>
-  </div>
+  </div>-->
 </template>
 
 <script>
+    // const BASEURL = "http://expresscargo.me/api";
 
     export default {
         name: 'TableView',
-        mounted() {
-            setTimeout ( ()=> {
-                $('#tableView').dataTable();
-            }, 100 )
-
+        props: ['paymentMethod', 'paymentMethods'],
+        data () {
+            return {
+                tableData: [],
+                originalData: [],
+                columns: ['seller', 'paymentMethod', 'nairaPrice', 'dollarPrice', 'limit'],
+                options: {
+                    perPage: 2,
+                    filterable: false
+                }
+            }
         },
+        watch: {
+            paymentMethod () {
+                if (this.paymentMethod === "all") {
+                    this.tableData = [...this.originalData];
+                    return;
+                }
+                let method = this.paymentMethods.find(meth => meth.id === Number(this.paymentMethod));
+                this.tableData = this.originalData.filter(data => {return data.paymentMethod === method.name});
+                // debugger
+            }
+        },
+        created () {
+            window.axios.get(`/api/offers/test-sell`)
+                .then(res => {
+                    // console.log([...res.data.data]);
+                    console.log(typeof res.data.data);
+                    let response = [];
+                    for (let data in res.data.data) {
+                        if (res.data.data.hasOwnProperty(data))
+                            response.push(res.data.data[data]);
+                    }
+                    response.forEach (data => {
+                        let seller = data.user.name;
+                        let paymentMethod = data.payment_method;
+                        let nairaPrice = 20000000;
+                        let dollarPrice = 1000;
+                        let maxAmount = data.max_amount;
+                        let minAmount = data.min_amount;
+                        this.tableData.push({seller, paymentMethod, nairaPrice, dollarPrice, limit: `${minAmount} - ${maxAmount}`});
+                        this.originalData =[...this.tableData];
+                    })
+
+
+
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+            ;
+        }
     };
 </script>
 
--- resources/assets/js/landing.js
+++ resources/assets/js/landing.js
@@ -4,6 +4,9 @@ window.Vue = require('vue');
 
 import router from './route'
 import Landing from './components/App'
+import {ClientTable} from 'vue-tables-2';
+
+Vue.use(ClientTable);
 
 window.Landing = new Vue({
     el: '#app',
