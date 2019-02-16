5c66847a6527de8fb5072b4347619c1fa1a79f29
James John James <jamesjay4199@gmail.com>
2019-02-16 11:25:33 +0100

    referral validation and verification

--- app/Traits/ManageReferrals.php
+++ app/Traits/ManageReferrals.php
@@ -8,8 +8,8 @@
 
 namespace App\Traits;
 
-//const TRADE_CURRENCY = "USD";
-const MINIMUM_TRADE_AMOUNT = 1300;
+const TRADE_COIN = "BTC";
+const MINIMUM_TRADE_AMOUNT = 10;
 
 use App\Models\Referral;
 use App\Models\User;
@@ -38,21 +38,60 @@ trait ManageReferrals
     public function validateReferral($user_id)
     {
         $user = User::find($user_id);
-        $amount = $user->trades()->sum('amount');
 
-        /*
-         * Check if user has traded up to the specified minimum trade amount
-         */
-        if ($amount < MINIMUM_TRADE_AMOUNT) {
+        $referral = Referral::where('user_id', $user_id)->first();
+        if (is_null($referral) || $referral->is_verified) {
             return;
         }
 
-        $referral = Referral::where('user_id', $user_id)->first();
-        if (is_null($referral)) {
+        if ( !$this->isValidated($user) ) {
             return;
         }
 
         $referral->is_verified = true;
         $referral->save();
+    }
+
+    /**
+     * Checks if user is validated
+     *
+     * @param User $user
+     * @return bool
+     */
+    public function isValidated(User $user)
+    {
+       return $this->isUserVerified($user) && $this->hasCompletedTrades($user);
+    }
+
+    /**
+     * Checks if user has passed all the verification tests
+     *
+     * @param User $user
+     * @return bool
+     */
+    private function isUserVerified (User $user)
+    {
+        return $user->identityDetails && $user->verified_phone && $user->verified;
+    }
+
+    /**
+     * Checks if user has completed required amount of trades
+     *
+     * @param User $user
+     * @return bool
+     */
+    private function hasCompletedTrades (User $user)
+    {
+        return $user->trades->count() >= $this->getMinimumTradeAmount();
+    }
+
+    /**
+     * Get minimum trade amount
+     *
+     * @return integer
+     */
+    private function getMinimumTradeAmount()
+    {
+        return env('MINIMUM_TRADE_AMOUNT', MINIMUM_TRADE_AMOUNT);
     }
 }
