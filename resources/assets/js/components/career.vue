4259d3ef1adc9a4906ed88062e87d78b5031505b
nedyudombat <nedyudombat@gmail.com>
2019-02-20 00:06:17 +0100

    external pages

--- resources/assets/js/components/career.vue
+++ resources/assets/js/components/career.vue
@@ -1,9 +1,74 @@
 <template>
   <div class="">
-    <p class="coming-soon-text">COMING SOON!</p>
-    <p class="error-message">Oh No! Seems this page is not available yet</p>
-    <div class="" style="width:100%; text-align:center; margin-bottom:20px;">
-      <a href="#">Go Back</a>
+    <div class="containers">
+      <div class="career-inner">
+        <p class="text-center">Lorem Ipsum Dolor</p>
+        <p class="text-center">sit amet, consectetur adipiscing elit. Integer lacinia, est eleifend scelerisque tristique, ante neque sodales felis, in</p>
+
+        <div class="article">
+          <p class="title">WHat We Offer</p>
+          <p class="subtitle">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia, est eleifend scelerisque tristique, ante neque sodales felis,
+            in placerat nisl dui quis odio. Mauris placerat, risus at fermentum dictum.<br><br>
+            Nibh diam sodales neque, at elementum nisl nibh vitae ex. Aenean dapibus rutrum dolor sit amet molestie. Donec mattis mi diam,
+            id malesuada arcu feugiat ac. Donec pellentesque accumsan lacus eu cursus. Etiam ut turpis lobortis, viverra est in, interdum risus.
+          </p>
+        </div>
+
+        <div class="article">
+          <p class="title">Explore Our Current Job Opportunities</p>
+          <p class="subtitle">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia, est eleifend scelerisque tristique, ante neque sodales felis,
+             placerat nisl dui quis odio. Mauris placerat, risus at fermentum dictum, nibh diam sodales neque, at elementum nisl nibh vitae ex.
+          </p>
+        </div>
+
+        <div class="career-flex-tab container-fluid">
+          <div class="row">
+            <div class="left col-xs-12 col-lg-4">
+              <p class="title">Job Openings</p>
+              <div class="d-flex justify-content-center">
+                <div class="dash"></div>
+              </div>
+              <div class="list-group">
+                <a href="#" class="list-group-item active">Cras justo odio</a>
+                <a href="#" class="list-group-item">Dapibus ac facilisis in</a>
+                <a href="#" class="list-group-item">Morbi leo risus</a>
+                <a href="#" class="list-group-item">Porta ac consectetur ac</a>
+                <a href="#" class="list-group-item">Vestibulum at eros</a>
+              </div>
+            </div>
+            <div class="right col-xs-12 col-lg-8">
+              <div class="article">
+                <p class="title">Business Development</p>
+                <p class="subtitle">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia, est eleifend scelerisque placerat nisl dui quis odio.
+                  Mauris placerat, risus at fermentum dictum, nibh diam sodales neque, at elementum nisl nibh vitae ex.<br><br>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia, est eleifend scelerisque tristique,
+                  ante neque sodales felis, in placerat nisl dui quis odio. Mauris placerat, risus at fermentum dictum, nibh diam sodales neque,
+                  at elementum nisl nibh vitae ex.<br><br>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia, est eleifend scel felis, in placerat nisl dui quis odio.
+                  ibh vitae ex.<br><br>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lacinia, est eleifend scelerisque tristique,
+                  ante neque sodales felis, in placerat nisl dui quis odio. Mauris placerat, risus at fermentum dictum, nibh diam sodales neque,
+                  at elementum nisl nibh vitae ex.<br><br>
+                  Send CV to <a href="mailto:jobs@moex.com">jobs@moex.com</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="container-fluid bg">
+      <div class="">
+        <p class="bg-text">Start Trading</p>
+        <p class="bg-text-2"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis luctus sodales. </p>
+        <div align="center">
+          <a class="text-color btn btn-default btn-lg button-sign" style="color:#6C63FF !important" href="#">Sign Up</a>
+        </div>
+      </div>
     </div>
   </div>
 </template>
