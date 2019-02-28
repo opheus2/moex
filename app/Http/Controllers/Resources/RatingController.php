540bda2e64ba9361f1b842dd8c44d5f43f01fb93
James John James <jamesjay4199@gmail.com>
2019-02-28 14:21:27 +0100

    work on table

---
+++ app/Http/Controllers/Resources/RatingController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Resources;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class RatingController extends Controller
+{
+    public function getAvgRating ($id) {
+        // TODO: get average user rating from db;
+    }
+}
