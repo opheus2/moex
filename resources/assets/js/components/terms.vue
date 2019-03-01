1885cf29d6886faba59a1be431fdf8ad2766e9ea
opheus2 <j.ominiabohs@gmail.com>
2019-03-01 16:15:38 -0500

    added content to terms page

--- resources/assets/js/components/terms.vue
+++ resources/assets/js/components/terms.vue
@@ -35,52 +35,89 @@
                 <h5 class="service">Terms of Service</h5>
             </div>
             <div class="row">
-                <p class="sub">Using MOEX</p>
-                <p class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis luctus sodales. Mauris sem quam, commodo vel leo sit amet, egestas congue nibh. Nulla non velit non ipsum vehicula condimentum a non felis. Quisque augue eros, venenatis quis luctus sed, vestibulum in lacus. Vivamus sed dolor vitae diam commodo euismod. Suspendisse quis aliquet ex, vitae sagittis tortor. Curabitur congue lectus quis euismod facilisis. Vestibulum ac diam pulvinar lacus lacinia rutrum ac at mi. Sed at malesuada ipsum.</p>
+                <p class="sub"><b>ACCEPTANCE</b></p>
+                <p class="content">The following Terms and Conditions and the Policies (together, the “Terms”) apply to any person that registers for and/or opens a MOEX Account through <a href="https://moexchange.io">moexchange.io</a> or any associated mobile applications, website, or APIs (together, the “MOEX Site”)</p>
 
-                <p class="content">Aenean posuere urna ac nisi condimentum, et semper ligula cursus. Proin eget nunc at magna interdum semper vel ac magna. Fusce nulla magna, pellentesque sit amet pretium ut, commodo et ligula. Sed convallis tempor massa id molestie. Morbi id laoreet arcu, eu lobortis ex. Morbi dignissim, ligula nec facilisis lobortis, lectus diam dapibus elit, et posuere tellus augue id sem. Nunc tincidunt mauris justo, ac pulvinar leo accumsan in. Proin id aliquam ex, at feugiat lacus.</p>
+                <p class="content">The Terms constitute a legally binding agreement between you and MY OWN CONCEPTS WORLDWIDE LTD., which is a company incorporated under the laws of Nigeria. For further information on MY OWN CONCEPTS WORLDWIDE LTD., and its operating subsidiaries, please see the Company Information page on the MOEX website. For the purposes of these Terms, any reference to “we” “us” “our” “MOEX” and/or any similar term shall be construed as reference to MY OWN CONCEPTS WORLDWIDE LTD.</p>
+
+                <p class="content">By registering for and opening a MOEX Account, you unconditionally accept these Terms and agree to be bound by and act in accordance with them. You also accept and agree that you are solely responsible for understanding and complying with all laws, rules, regulations and requirements of the jurisdiction in which you live that may be applicable to your use of the MOEX Site and/or your MOEX Account, including but not limited to, those related to export or import activity, taxes or foreign currency transactions.
+                </p>
+
+                <p class="content">Please read these Terms carefully before using the MOEX Site because they affect your legal rights and obligations.</p>
             </div>
 
             <div class="row">
-                <p class="sub">Copyright Policy</p>
-                <p class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis luctus sodales. Mauris sem quam, commodo vel leo sit amet, egestas congue nibh. Nulla non velit non ipsum vehicula condimentum a non felis. Quisque augue eros, venenatis quis luctus sed, vestibulum in lacus. Vivamus sed dolor vitae diam commodo euismod. Suspendisse quis aliquet ex, vitae sagittis tortor. Curabitur congue lectus quis euismod facilisis. Vestibulum ac diam pulvinar lacus lacinia rutrum ac at mi. Sed at malesuada ipsum.</p>
-
-            </div>
-            <div class="row">
-                <p class="sub">Security</p>
-                <p class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis luctus sodales. Mauris sem quam, commodo vel leo sit amet, egestas congue nibh. Nulla non velit non ipsum vehicula condimentum a non felis. Quisque augue eros, venenatis quis luctus sed, vestibulum in lacus. Vivamus sed dolor vitae diam commodo euismod. Suspendisse quis aliquet ex, vitae sagittis tortor. Curabitur congue lectus quis euismod facilisis. Vestibulum ac diam pulvinar lacus lacinia rutrum ac at mi. Sed at malesuada ipsum.</p>
-
-            </div>
-
-            <div class="row">
-                <p class="sub"><b>Third-Party Links, Sites, and Services</b></p>
-                <p class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis luctus sodales. Mauris sem quam, commodo vel leo sit amet, egestas congue nibh. Nulla non velit non ipsum vehicula condimentum a non felis. Quisque augue eros, venenatis quis luctus sed, vestibulum in lacus. Vivamus sed dolor vitae diam commodo euismod. Suspendisse quis aliquet ex, vitae sagittis tortor. Curabitur congue lectus quis euismod facilisis. Vestibulum ac diam pulvinar lacus lacinia rutrum ac at mi. Sed at malesuada ipsum.
-                Aenean posuere urna ac nisi condimentum, et semper ligula cursus. Proin eget nunc at magna interdum semper vel ac magna. Fusce nulla magna, pellentesque sit amet pretium ut, commodo et ligula. Sed convallis tempor massa id molestie. Morbi id laoreet arcu, eu lobortis ex. Morbi dignissim, ligula nec facilisis lobortis, lectus diam dapibus elit, et posuere tellus augue id sem. Nunc tincidunt mauris justo, ac pulvinar leo accumsan in. Proin id aliquam ex, at feugiat lacus.</p>
-
-            </div>
-            <div class="row">
-                <p class="sub"><b>Termination</b></p>
-                <p class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis luctus sodales. Mauris sem quam, commodo vel leo sit amet, egestas congue nibh. Nulla non velit non ipsum vehicula condimentum a non felis. Quisque augue eros, venenatis quis luctus sed, vestibulum in lacus. Vivamus sed dolor vitae diam commodo euismod. Suspendisse quis aliquet ex, vitae sagittis tortor. Curabitur congue lectus quis euismod facilisis. Vestibulum ac diam pulvinar lacus lacinia rutrum ac at mi. Sed at malesuada ipsum.</p>
-
-            </div>
-            <div class="row">
-                <p class="sub"><b>Indemnity</b></p>
-                <p class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis luctus sodales. Mauris sem quam, commodo vel leo sit amet, egestas congue nibh. Nulla non velit non ipsum vehicula condimentum a non felis. Quisque augue eros, venenatis quis luctus sed, vestibulum in lacus. Vivamus sed dolor vitae diam commodo euismod. Suspendisse quis aliquet ex, vitae sagittis tortor. Curabitur congue lectus quis euismod facilisis. Vestibulum ac diam pulvinar lacus lacinia rutrum ac at mi. Sed at malesuada ipsum.
-                Aenean posuere urna ac nisi condimentum, et semper ligula cursus. Proin eget nunc at magna interdum semper vel ac magna. Fusce nulla magna, pellentesque sit amet pretium ut, commodo et ligula. Sed convallis tempor massa id molestie. Morbi id laoreet arcu, eu lobortis ex. Morbi dignissim, ligula nec facilisis lobortis, lectus diam dapibus elit, et posuere tellus augue id sem. Nunc tincidunt mauris justo, ac pulvinar leo accumsan in. Proin id aliquam ex, at feugiat lacus.</p>
+                <p class="sub"><b>1. AMENDMENT OF TERMS</b></p>
+                <p class="content">a. MOEX may amend the Terms from time to time. You should visit the MOEX website regularly to check when the Terms were last updated (as displayed at the top of this document) and to review the current Terms. We will do our best to notify you of any amendments to the Terms that we consider likely to materially affect your rights and obligations. Any such notice will be posted on the MOEX Site, or sent by email to the address associated with your MOEX Account view <a>Electronic Communications</a></p>
+                
+                <p class="content">b. The continued use of your MOEX Account, after any amendment to these Terms, constitutes your acceptance of the Terms, as modified by such amendment. If you do not accept the Terms, or any amendment to them, you must immediately stop using the MOEX Site and your MOEX Account.</p>
 
             </div>
 
             <div class="row">
-                <p class="sub"><b>Disclaimers</b></p>
-                <p class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis luctus sodales. Mauris sem quam, commodo vel leo sit amet, egestas congue nibh. Nulla non velit non ipsum vehicula condimentum a non felis. Quisque augue eros, venenatis quis luctus sed, vestibulum in lacus. Vivamus sed dolor vitae diam commodo euismod. Suspendisse quis aliquet ex, vitae sagittis tortor. Curabitur congue lectus quis euismod facilisis. Vestibulum ac diam pulvinar lacus lacinia rutrum ac at mi. Sed at malesuada ipsum.
-                Aenean posuere urna ac nisi condimentum, et semper ligula cursus. Proin eget nunc at magna interdum semper vel ac magna. Fusce nulla magna, pellentesque sit amet pretium ut, commodo et ligula. Sed convallis tempor massa id molestie. Morbi id laoreet arcu, eu lobortis ex. Morbi dignissim, ligula nec facilisis lobortis, lectus diam dapibus elit, et posuere tellus augue id sem. Nunc tincidunt mauris justo, ac pulvinar leo accumsan in. Proin id aliquam ex, at feugiat lacus.</p>
+                <p class="sub"><b>2. ELIGIBILITY</b></p>
+                <p class="content">a. By opening a MOEX Account, you expressly warrant and represent that:<br>
+                1. You are 18 years of age or over and have full capacity to accept the Terms and enter any transaction available through the MOEX Site;<br>
+                2. You will not open, or attempt to open, a MOEX Account under any name except your own; or use your MOEX Account to carry out transactions on behalf of a third party;<br>
+                3. You will follow the <a>MOEX Business account application process</a> if you seek to open and use a MOEX Account for a non-individual legal entity (e.g. a company, trust or partnership);<br>
+                4. You will not have more than one MOEX Account; use or access any MOEX Account other than your own; or assist any other person in obtaining unauthorised access to any MOEX Account;<br>
+                5. You will provide security for transactions carried out through your MOEX Account by ensuring there is sufficient Local Currency or cryptocurrency (as the case may be) in your MOEX Account in advance of any transaction;
+                6. You will not allow or facilitate the deposit of cryptocurrency into your MOEX Account unless that deposit is made in accordance with the Deposit Requirements (view <a>Deposits</a>);
+                7. You will not allow or facilitate the withdrawal of cryptocurrency from your MOEX Wallet to a bank account held in the name of a third party (see <a>Withdrawals</a>);
+                8. You will not use your MOEX Account for or in relation to any illegal activity, any <a>Prohibited Activity</a>, in violation of any laws, statutes, ordinances or regulations;
+
+
+                </p>
+                
+                <p class="content">b. By opening a MOEX Account you accept and agree that MOEX may, without further notice and in its sole discretion, terminate, suspend or restrict the account of any customer who uses, or who we reasonably suspect may be using, the MOEX Site or any MOEX Account in a manner that is inconsistent with the letter or spirit of these Terms.</p>
+
+            </div>
+            <div class="row">
+                <p class="sub"><b>RISKS</b></p>
+                <p class="content">a.Before using the MOEX Site, you should ensure that you understand the risks involved in buying, selling or trading cryptocurrencies. Cryptocurrency markets can be volatile and prices can fluctuate significantly, which could result in sudden and significant increases or decreases in the value of your assets. This market volatility and other risks involved in buying, selling or trading cryptocurrencies are explained on the <a>risk warning</a> page of the MOEX website. There may be additional risks not identified in these Terms or in the <a>risk warning</a>.</p>
+                
+                <p class="content">b. You should carefully assess whether your financial situation and risk tolerance is suitable for buying, selling or trading cryptocurrency. You accept and agree that you are solely responsible for any decision to buy, sell, trade or otherwise hold or deal with cryptocurrency.
+</p>
+
+            </div>
+            <div class="row">
+                <p class="sub"><b>ELECTRONIC COMMUNICATIONS</b></p>
+                <p class="content">a. You accept and agree that: <br> 
+                1. any communications, agreements, notices and/or any other documents (together “Communications”) relating to your MOEX Account or your use of MOEX’s services will be provided to you electronically by posting them on the MOEX Site, emailing them to the email address you have provided to us, or through any other form of electronic communication. You consent to receiving all Communications electronically;<br>
+                2. you will at all times have available to you the necessary hardware and software to receive, access and retain Communications sent to you electronically, including a device with an internet connection and a valid and accessible email address;<br>
+                3. you assume full responsibility for providing MOEX with a valid and accessible email address to which any Communications may be sent, and for ensuring that email address and any other contact information is kept up to date. Any Communication sent to the email address you have provided to us will be deemed to have been received by you. You can amend your contact information by signing-in to your MOEX Account and accessing the Settings page.
+                </p>
+                
+                <p class="content">b. You may at any time withdraw your consent to receiving Communications electronically by accessing the Settings Page. You acknowledge that failure to give, or withdrawing, consent to receiving Communications electronically puts the security of your MOEX Account at risk (see <a>Account Security</a>) and that MOEX reserves the right to close your MOEX Account in the event you fail to give, or withdraw, your consent to the receipt of Communications electronically.
+.
+                </p>
 
             </div>
 
             <div class="row">
-                <p class="sub"><b>Governing and Jurisdiction</b></p>
-                <p class="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sagittis luctus sodales. Mauris sem quam, commodo vel leo sit amet, egestas congue nibh. Nulla non velit non ipsum vehicula condimentum a non felis. Quisque augue eros, venenatis quis luctus sed, vestibulum in lacus. Vivamus sed dolor vitae diam commodo euismod. Suspendisse quis aliquet ex, vitae sagittis tortor. Curabitur congue lectus quis euismod facilisis. Vestibulum ac diam pulvinar lacus lacinia rutrum ac at mi. Sed at malesuada ipsum.
-                Aenean posuere urna ac nisi condimentum, et semper ligula cursus. Proin eget nunc at magna interdum semper vel ac magna. Fusce nulla magna, pellentesque sit amet pretium ut, commodo et ligula. Sed convallis tempor massa id molestie. Morbi id laoreet arcu, eu lobortis ex. Morbi dignissim, ligula nec facilisis lobortis, lectus diam dapibus elit, et posuere tellus augue id sem. Nunc tincidunt mauris justo, ac pulvinar leo accumsan in. Proin id aliquam ex, at feugiat lacus.</p>
+                <p class="sub"><b>IDENTITY VERIFICATION</b></p>
+                <p class="content">a. MOEX implements and maintains the highest standards of Know Your Customer (“KYC”) processes and controls as part of our commitment to combating fraud and assisting in the prevention of money laundering and terrorist financing. While our industry is largely unregulated, MOEX voluntarily adheres to local and international compliance standards in relation to customer due diligence, as explained on the <a>Compliance</a> page of the MOEX website.</p>
+                
+                <p class="content">b. To ensure we meet these standards, our customers are required to provide certain personal details and documents when opening a MOEX Account (“Identity Verification”). The nature and extent of the Identity Verification required will depend upon which of our supported countries you are resident in, and the deposit and withdrawal limits that you wish to apply to your MOEX Account. In certain circumstances, MOEX may also perform enhanced due diligence (“EDD”) procedures in relation to your MOEX Account. You accept and agree that you will remain subject to such procedures at all times.</p>
+                
+                <p class="content">c. MOEX reserves the right to, at any time:<br>
+                1. restrict or suspend your MOEX Account when we, in our sole discretion, consider it necessary to carry out further Identity Verification and/or EDD; or
+                2. terminate your MOEX Account if you provide, or we suspect you have provided, false information or refuse to provide information we require for Identity Verification and/or EDD.
+                </p>
+                
+                <p class="content">d. You accept and agree that there may be delays in accessing your MOEX Account, or in carrying out transactions through your MOEX Account, while we undertake any Identity Verification and/or EDD procedures.
+                </p>
+
+            </div>
+
+            <div class="row">
+                <p class="sub"><b>THE MOEX WALLET</b></p>
+                <p class="content">a. The MOEX Wallet allows you to send, receive and store cryptocurrency (together, “MOEX Wallet Transactions”)</p>
+                
+                <p class="content">b. <b>Supported Cryptocurrencies.</b> The MOEX Wallet is only available in relation to the cryptocurrencies that MOEX, in its sole discretion, decides to support (“Supported Cryptocurrency” or “Supported Cryptocurrencies”). Information about the Supported Cryptocurrencies can be found on the <a>Supported Cryptocurrency</a> page on the MOEX website. The Supported Cryptocurrencies may change from time to time. Under no circumstances should you attempt to carry out a MOEX Wallet Transaction in relation to a cryptocurrency other than a Supported Cryptocurrency. In particular, but without limiting the generality of the foregoing, you accept and agree that you will have no access, entitlement or claim: <br>
+                1. to any cryptocurrency sent to a receive address associated with your MOEX Wallet where that cryptocurrency is not a Supported Cryptocurrency; or <br>
+                2. to any cryptocurrency sent to a receive address associated with your MOEX Wallet, where that receive address is associated with another cryptocurrency. For example, you will have no access, entitlement or claim to any Bitcoin Cash (BCH) sent to a Bitcoin (BTC) receive address.
+                </p>
 
             </div>
 
