3ebc0e06c60d6958c77d401fb5c43c0c9f970687
opheus2 <31169044+opheus2@users.noreply.github.com>
2019-03-31 18:33:29 -0500

    Revert "Revert "added fix for login history""

--- app/Models/LoginLog.php
+++ app/Models/LoginLog.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class LoginLog extends Model
 {
-    protected $fillable = ['user_id', 'ip_address', 'logged_at', 'useragent', 'referrer', 'is_first_time'];
+    protected $fillable = ['user_id', 'ip_address', 'logged_at', 'useragent', 'referrer', 'is_first_time', 'location', 'isp'];
 }
