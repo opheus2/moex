245924cfc37750f27417abd0e9193826fbd309dc
James John James <jamesjay4199@gmail.com>
2019-04-10 00:26:33 +0100

    fixed bug on landing page

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -37,7 +37,7 @@
     </body>
 
 <script>
-    window._isOffline = {{is_null(auth::user()) }}
+    window._isOffline = {{is_null(auth::user()) ? 1 : 0 }}
 </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
